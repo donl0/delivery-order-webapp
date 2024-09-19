5a01eb1d580569274adefc192356cd534126dd43
donl0 <donl_117n@mail.ru>
2024-09-19 18:43:24 +0300

    Add DbContext

---
+++ backend/DeliveryOrderService/Infrastructure/Db/OrderDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Db
+{
+    public class OrderDbContext : DbContext, IDbContext
+    {
+        public DbSet<Order> Orders { get; }
+
+        public OrderDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ backend/DeliveryOrderService/Infrastructure/Interfaces/IDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IDbContext
+    {
+        public DbSet<Order> Orders { get; }
+    }
+}
