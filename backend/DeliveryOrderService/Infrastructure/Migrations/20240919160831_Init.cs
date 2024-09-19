a2f210e07d1ec23b4576e3308f2b3186517c6f5a
donl0 <donl_117n@mail.ru>
2024-09-19 19:10:27 +0300

    Add migrations

---
+++ backend/DeliveryOrderService/Infrastructure/Migrations/20240919160831_Init.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SenderCity = table.Column<string>(type: "text", nullable: false),
+                    SenderAddress = table.Column<string>(type: "text", nullable: false),
+                    RecipientCity = table.Column<string>(type: "text", nullable: false),
+                    RecipientAddress = table.Column<string>(type: "text", nullable: false),
+                    CargoWeight = table.Column<long>(type: "bigint", nullable: false),
+                    CargoPickupDate = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
