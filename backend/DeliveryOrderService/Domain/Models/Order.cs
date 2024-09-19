fc0e25bea7c3dd91d7a807721ecd2effe7513e9f
donl0 <donl_117n@mail.ru>
2024-09-19 18:05:45 +0300

    Add Order model

---
+++ backend/DeliveryOrderService/Domain/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        [Required]
+        public string SenderCity { get; set; }
+        [Required]
+        public string SenderAddress { get; set; }
+        [Required]
+        public string RecipientCity { get; set; }
+        [Required]
+        public string RecipientAddress { get; set; }
+        [Required]
+        public uint CargoWeight { get; set; }
+        [Required]
+        public string CargoPickupDate { get; set;}
+    }
+}
