c1857ee38f0a41e4eb4fe1ff19f7e90eb2edeae3
donl0 <donl_117n@mail.ru>
2024-09-19 19:38:27 +0300

    Add dto to create Order

---
+++ backend/DeliveryOrderService/Application/DTOs/OrderRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs
+{
+    public class OrderRequestDTO
+    {
+        [Required]
+        public string SenderCity { get; set; }
+        [Required]
+        public string SenderAddress { get; set; }
+        [Required]
+        public string RecipientCity { get; set; }
+        [Required]
+        public string RecipientAddress { get; set; }
+        [Required]
+        public uint CargoWeight { get; set; }
+        [Required]
+        public string CargoPickupDate { get; set; }
+    }
+}
