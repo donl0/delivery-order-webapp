146ebea85e30a8d392a218294bd473caf3e23c04
donl0 <donl_117n@mail.ru>
2024-09-20 18:10:13 +0300

    Add api types

---
+++ frontend/src/types/Order.ts
@@ -0,0 +1,12 @@
+export interface PersonOrderEntity {
+    city: string;
+    address: string;
+}
+
+export interface Order {
+    sender: PersonOrderEntity;
+    recipient: PersonOrderEntity;
+    cargoWeight: number;
+    orderNumber: string;
+    cargoPickupDate: string;
+}
