d349736f4ae1696b045b888115e76bd3bb16f7d3
donl0 <donl_117n@mail.ru>
2024-09-20 12:26:22 +0300

    Add global exception handler

---
+++ backend/DeliveryOrderService/DeliveryOrderService/Middlewares/GlobalExeptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryOrderService.Middlewares
+{
+    public class GlobalExeptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExeptionMiddleware> _logger;
+
+        public GlobalExeptionMiddleware(RequestDelegate next, ILogger<GlobalExeptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (DomainException ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                var problem = new ObjectResult(new { Error = ex.Message })
+                {
+                    StatusCode = StatusCodes.Status400BadRequest
+                };
+
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+                await context.Response.WriteAsJsonAsync(problem);
+
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+        }
+
+    }
+}
--- backend/DeliveryOrderService/DeliveryOrderService/Program.cs
+++ backend/DeliveryOrderService/DeliveryOrderService/Program.cs
@@ -1,3 +1,4 @@
+using DeliveryOrderService.Middlewares;
 using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<GlobalExeptionMiddleware>();
 
 app.UseHttpsRedirection();
 
