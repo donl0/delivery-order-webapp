4c83998ccc6226276f51f4b7078fa54b60197898
donl0 <donl_117n@mail.ru>
2024-09-20 17:36:32 +0300

    Make correct migrations

--- backend/DeliveryOrderService/Infrastructure/Migrations/20240920143326_init.cs
+++ backend/DeliveryOrderService/Infrastructure/Migrations/20240920143326_init.cs
@@ -7,7 +7,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class init : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
