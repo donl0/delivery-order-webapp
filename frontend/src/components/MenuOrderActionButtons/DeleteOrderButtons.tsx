e0316d46f215718c7c4a8b8990d787b510e659a8
donl0 <donl_117n@mail.ru>
2024-09-22 01:06:58 +0300

    Add delete order button

---
+++ frontend/src/components/MenuOrderActionButtons/DeleteOrderButtons.tsx
@@ -0,0 +1,18 @@
+import { OrderId } from "../../types/Order";
+import BaseOrderButton from "../UI/orderActoinButton/BaseOrderButton";
+import { FC } from "react";
+
+const DeleteOrderButton : FC<OrderId> = ({id}) => {
+
+    const handleClick = () => {
+        console.log({id} + "deleted")
+      };
+    
+      return (
+        <BaseOrderButton color="red" onClick={handleClick}>
+          Delete
+        </BaseOrderButton>
+      );
+}
+
+export default DeleteOrderButton;
