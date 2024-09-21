8bb2a84ec39e675dcf3824755239d34091c7cfe3
donl0 <donl_117n@mail.ru>
2024-09-21 15:17:04 +0300

    Add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Orders from "./components/Orders/Orders";
+import CreateOrderForm from "./components/orderForms/CreateOrderForm/CreateOrderForm";
+import { OrderProvider } from './components/OrderContext/CurrentOrderProvider';
+import ViewOrderDetailsForm from './components/orderForms/ViewOrderDetailsForm/ViewOrderDetailsForm';
+
 const App = () => {
   return (
-    <div>
-    </div>
+    <OrderProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Orders></Orders>}></Route>
+          <Route path="/create-order" element={<CreateOrderForm></CreateOrderForm>}></Route>
+          <Route path="/see-order-detail" element={<ViewOrderDetailsForm></ViewOrderDetailsForm>}></Route>
+        </Routes>
+      </Router>
+    </OrderProvider>
   )
-
 }
 
 export default App;
