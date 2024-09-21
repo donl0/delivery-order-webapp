070b2b9bee50b69a2c725a03e59f97cd4e28e69b
donl0 <donl_117n@mail.ru>
2024-09-22 01:07:14 +0300

    Add create order form button opener

---
+++ frontend/src/components/MenuOrderActionButtons/CreateOrderFormOpenerButton.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { useNavigateToUrl} from "../../utils/orderUtils";
+import BaseOrderButton from "../UI/orderActoinButton/BaseOrderButton";
+
+interface CreateOrderFormOpenerButtonProps {
+}
+
+const CreateOrderFormOpenerButton: FC<CreateOrderFormOpenerButtonProps> = () => {
+  const navigateToUrl = useNavigateToUrl();
+
+  const handleClick = () => {
+    navigateToUrl(`/create-order`);
+  };
+
+  return (
+    <BaseOrderButton color="green" size="large" onClick={handleClick}>
+      Create Order
+    </BaseOrderButton>
+  );
+};
+
+export default CreateOrderFormOpenerButton;
