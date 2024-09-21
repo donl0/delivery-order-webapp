11a8db500656fb5f95438062a04b16524ccc1c3f
donl0 <donl_117n@mail.ru>
2024-09-21 15:23:46 +0300

    Delete form from base order component

--- frontend/src/components/orderForms/BaseOrderForm/BaseOrderForm.tsx
+++ frontend/src/components/orderForms/BaseOrderForm/BaseOrderForm.tsx
@@ -22,70 +22,68 @@ const BaseOrderForm: FC<BaseOrderFormProps> = ({
 }) => {
   return (
     <div>
-      <form>
-        {orderNumber && (
-          <FormInputField
-            label="Order number"
-            type="text"
-            defaultValue={orderNumber}
-            isReadOnly={true}
-          />
-        )}
-
+      {orderNumber && (
         <FormInputField
-          label="Sender's city"
+          label="Order number"
           type="text"
-          defaultValue={sender.city}
-          isReadOnly={isReadOnly}
-          register={register ? register("sender.city", { required: "Sender city is required" }) : undefined}
-          errorMessage={errors?.sender?.city?.message}
+          defaultValue={orderNumber}
+          isReadOnly={true}
         />
+      )}
 
-        <FormInputField
-          label="Sender's address"
-          type="text"
-          defaultValue={sender.address}
-          isReadOnly={isReadOnly}
-          register={register ? register("sender.address", { required: "Sender address is required" }) : undefined}
-          errorMessage={errors?.sender?.address?.message}
-        />
+      <FormInputField
+        label="Sender's city"
+        type="text"
+        defaultValue={sender.city}
+        isReadOnly={isReadOnly}
+        register={register ? register("sender.city", { required: "Sender city is required" }) : undefined}
+        errorMessage={errors?.sender?.city?.message}
+      />
 
-        <FormInputField
-          label="Recipient's city"
-          type="text"
-          defaultValue={recipient.city}
-          isReadOnly={isReadOnly}
-          register={register ? register("recipient.city", { required: "Recipient city is required" }) : undefined}
-          errorMessage={errors?.recipient?.city?.message}
-        />
+      <FormInputField
+        label="Sender's address"
+        type="text"
+        defaultValue={sender.address}
+        isReadOnly={isReadOnly}
+        register={register ? register("sender.address", { required: "Sender address is required" }) : undefined}
+        errorMessage={errors?.sender?.address?.message}
+      />
 
-        <FormInputField
-          label="Recipient's address"
-          type="text"
-          defaultValue={recipient.address}
-          isReadOnly={isReadOnly}
-          register={register ? register("recipient.address", { required: "Recipient address is required" }) : undefined}
-          errorMessage={errors?.recipient?.address?.message}
-        />
+      <FormInputField
+        label="Recipient's city"
+        type="text"
+        defaultValue={recipient.city}
+        isReadOnly={isReadOnly}
+        register={register ? register("recipient.city", { required: "Recipient city is required" }) : undefined}
+        errorMessage={errors?.recipient?.city?.message}
+      />
 
-        <FormInputField
-          label="Cargo's weight"
-          type="number"
-          defaultValue={cargoWeight}
-          isReadOnly={isReadOnly}
-          register={register ? register("cargoWeight", { required: "Weight is required", min: { value: 1, message: "Weight must be greater than 0" } }) : undefined}
-          errorMessage={errors?.cargoWeight?.message}
-        />
+      <FormInputField
+        label="Recipient's address"
+        type="text"
+        defaultValue={recipient.address}
+        isReadOnly={isReadOnly}
+        register={register ? register("recipient.address", { required: "Recipient address is required" }) : undefined}
+        errorMessage={errors?.recipient?.address?.message}
+      />
 
-        <FormInputField
-          label="Cargo pickup date"
-          type="date"
-          defaultValue={cargoPickupDate.toISOString().split("T")[0]}
-          isReadOnly={isReadOnly}
-          register={register ? register("cargoPickupDate", { required: "Cargo pickup date is required" }) : undefined}
-          errorMessage={errors?.cargoPickupDate?.message}
-        />
-      </form>
+      <FormInputField
+        label="Cargo's weight"
+        type="number"
+        defaultValue={cargoWeight}
+        isReadOnly={isReadOnly}
+        register={register ? register("cargoWeight", { required: "Weight is required", min: { value: 1, message: "Weight must be greater than 0" } }) : undefined}
+        errorMessage={errors?.cargoWeight?.message}
+      />
+
+      <FormInputField
+        label="Cargo pickup date"
+        type="date"
+        defaultValue={cargoPickupDate.toISOString().split("T")[0]}
+        isReadOnly={isReadOnly}
+        register={register ? register("cargoPickupDate", { required: "Cargo pickup date is required" }) : undefined}
+        errorMessage={errors?.cargoPickupDate?.message}
+      />
     </div>
   );
 };
