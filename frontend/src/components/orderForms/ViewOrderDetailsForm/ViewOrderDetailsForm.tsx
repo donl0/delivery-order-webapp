50678724672d03149dc20ddae63fbba8adaa7018
donl0 <donl_117n@mail.ru>
2024-09-21 15:16:20 +0300

    Add loading from provider to view details form element

--- frontend/src/components/orderForms/ViewOrderDetailsForm/ViewOrderDetailsForm.tsx
+++ frontend/src/components/orderForms/ViewOrderDetailsForm/ViewOrderDetailsForm.tsx
@@ -1,10 +1,40 @@
-import { FC } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import BaseOrderForm from "../BaseOrderForm/BaseOrderForm";
 import { Order } from "../../../types/Order";
+import { getOrder } from "../../../api/ordersApi";
+import { CurrentOrderContext } from "../../OrderContext/CurrentOrderProvider";
 
-const ViewOrderDetailsForm : FC <Order> = (props) => {
+
+const ViewOrderDetailsForm : FC  = () => {
+    const { orderId, setOrderId } = useContext(CurrentOrderContext);
+
+    const [order, setOrder] = useState<Order>();
+    const [loading, setLoading] = useState<boolean>(true);
+    
+    useEffect(() => {
+        const loadOrders = async () => {
+            try {
+                const order: Order = await getOrder(orderId);
+
+                setOrder(order);
+            }
+            catch {
+                setLoading(true);
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+
+        loadOrders();
+    }, [orderId])
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     return (
-        <BaseOrderForm {...props} isReadOnly={true}></BaseOrderForm>
+        <BaseOrderForm {...order} isReadOnly={true}></BaseOrderForm>
     )
 }
 
