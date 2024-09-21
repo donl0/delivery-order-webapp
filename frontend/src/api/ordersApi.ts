c83204d7e89966794428418cfcf44ce0ebc42071
donl0 <donl_117n@mail.ru>
2024-09-21 15:14:04 +0300

    Add id to orders stub api

--- frontend/src/api/ordersApi.ts
+++ frontend/src/api/ordersApi.ts
@@ -1,8 +1,28 @@
 import { Order } from '../types/Order';
 
+export const getOrder = async (id: number) : Promise<Order> => {
+    let order: Order = {
+        id: 1,
+            sender: {
+                city: "Saint-Petersburg",
+                address: "Nevsky",
+            },
+            recipient: {
+                city: "Saint-Petersburg",
+                address: "Nevsky",
+            },
+            cargoWeight: 10,
+            orderNumber: "asdasd32-23233",
+            cargoPickupDate: new Date()
+        };
+    
+    return order;
+}
+
 export const getOrders = async (): Promise<Order[]> => {
     let data: Order[] = [
         {
+            id: 1,
             sender: {
                 city: "Saint-Petersburg",
                 address: "Nevsky",
@@ -12,10 +32,11 @@ export const getOrders = async (): Promise<Order[]> => {
                 address: "Nevsky",
             },
             cargoWeight: 10,
-            orderNumber: "asdasd32-2323",
+            orderNumber: "asdasd32-23233",
             cargoPickupDate: new Date()
         },
         {
+            id: 2,
             sender: {
                 city: "Saint-Petersburg",
                 address: "Nevsky",
@@ -25,10 +46,11 @@ export const getOrders = async (): Promise<Order[]> => {
                 address: "Nevsky",
             },
             cargoWeight: 10,
-            orderNumber: "asdasd32-2323",
+            orderNumber: "asdasd32-23234",
             cargoPickupDate: new Date()
         },
         {
+            id: 3,
             sender: {
                 city: "Saint-Petersburg",
                 address: "Nevsky",
@@ -38,7 +60,7 @@ export const getOrders = async (): Promise<Order[]> => {
                 address: "Nevsky",
             },
             cargoWeight: 10,
-            orderNumber: "asdasd32-2323qweqwe",
+            orderNumber: "asdasd32-2323qweqwe5",
             cargoPickupDate: new Date()
         }]
 
