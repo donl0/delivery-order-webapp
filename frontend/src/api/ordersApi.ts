a008a90313e20d819d306368f1d9a0048944c3e3
donl0 <donl_117n@mail.ru>
2024-09-22 16:44:15 +0300

    Add update order action

--- frontend/src/api/ordersApi.ts
+++ frontend/src/api/ordersApi.ts
@@ -1,5 +1,36 @@
 import { Order } from '../types/Order';
 
+export const updateOrder = async (order: Order): Promise<void> => {
+    const url = 'https://localhost:7292/api/Orders';
+
+    const cargoPickupDate = typeof order.cargoPickupDate === 'string'
+        ? new Date(order.cargoPickupDate)
+        : order.cargoPickupDate;
+
+    const orderUpdateDto = {
+        id: order.id,
+        senderCity: order.sender.city,
+        senderAddress: order.sender.address,
+        recipientCity: order.recipient.city,
+        recipientAddress: order.recipient.address,
+        cargoWeight: order.cargoWeight,
+        cargoPickupDate: cargoPickupDate.toISOString()
+    };
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'accept': 'text/plain',
+        },
+        body: JSON.stringify(orderUpdateDto),
+    });
+
+    if (!response.ok) {
+        throw new Error('Update error');
+    }
+};
+
 export const getOrder = async (id: number): Promise<Order> => {
     const response = await fetch(`https://localhost:7292/api/Orders/${id}`);
 
@@ -9,7 +40,7 @@ export const getOrder = async (id: number): Promise<Order> => {
 
     const order: Order = await response.json();
     order.cargoPickupDate = new Date(order.cargoPickupDate)
-    
+
     return order;
 }
 
