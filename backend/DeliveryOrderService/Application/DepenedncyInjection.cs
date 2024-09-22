a61fdd4beca09552be879245b3e759f7c1533a63
donl0 <donl_117n@mail.ru>
2024-09-22 15:28:15 +0300

    Register mapper and mediatR

---
+++ backend/DeliveryOrderService/Application/DepenedncyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Application.CQRS.Orders.Queries.GetAllOrders;
+using Application.Interfaces;
+using Application.Mapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DepenedncyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services) {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetAllOrdersQuery).Assembly));
+            services.AddTransient<IMapper, OrderDtoMapper>();
+
+            return services;
+        }
+
+    }
+}
