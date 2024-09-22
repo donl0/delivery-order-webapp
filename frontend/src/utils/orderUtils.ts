8259b80ae32c629e9b339dc74ccb066f72d8bb15
donl0 <donl_117n@mail.ru>
2024-09-22 17:26:55 +0300

    Create map function that also helps to correctlyconvert date time from form

--- frontend/src/utils/orderUtils.ts
+++ frontend/src/utils/orderUtils.ts
@@ -1,8 +1,25 @@
 import { useNavigate } from "react-router-dom";
+import { Order } from "../types/Order";
 
 export const useNavigateToUrl = () => {
   const navigate = useNavigate();
   return (url: string) => {
     navigate(url);
+  };
+};
+
+export const mapOrderToDto = (order: Order) => {
+  const cargoPickupDate = typeof order.cargoPickupDate === 'string'
+      ? new Date(order.cargoPickupDate)
+      : order.cargoPickupDate;
+
+  return {
+      id: order.id,
+      senderCity: order.sender.city,
+      senderAddress: order.sender.address,
+      recipientCity: order.recipient.city,
+      recipientAddress: order.recipient.address,
+      cargoWeight: order.cargoWeight,
+      cargoPickupDate: cargoPickupDate.toISOString(),
   };
 };
