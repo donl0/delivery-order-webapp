16b603aec5e61dd82957d2c3a806d53d24427166
donl0 <donl_117n@mail.ru>
2024-09-22 11:56:18 +0300

    Add enum urls

---
+++ frontend/src/types/Urls.ts
@@ -0,0 +1,9 @@
+export const enum Urls{
+    HomePage = "/",
+    CreateOrder ="/create-order",
+    SeeDetail = "/see-order-detail",
+    EditOrder = "/edit-order"
+
+}
+
+console.log(Urls.HomePage);
