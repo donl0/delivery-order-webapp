354bb0b479501534aaae018bedc542e114461661
donl0 <donl_117n@mail.ru>
2024-09-22 15:30:30 +0300

    Add get order via mediator

---
+++ backend/DeliveryOrderService/Application/CQRS/Orders/Queries/GetOrder/GetOrderQuery.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+using MediatR;
+
+namespace Application.CQRS.Orders.Queries.GetOrder
+{
+    public class GetOrderQuery : IRequest<Order>
+    {
+        public long Id { get; set; }
+    }
+}
---
+++ backend/DeliveryOrderService/Application/CQRS/Orders/Queries/GetOrder/GetOrderQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using Domain.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.CQRS.Orders.Queries.GetOrder
+{
+    public class GetOrderQueryHandler : IRequestHandler<GetOrderQuery, Order>
+    {
+        private readonly IDbContext _context;
+
+        public GetOrderQueryHandler(IDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> Handle(GetOrderQuery request, CancellationToken cancellationToken)
+        {
+            Order order = await _context.Orders.FirstOrDefaultAsync(o => o.Id == request.Id);
+
+            if (order == null)
+            {
+                throw new NotFoundException($"Order with ID {request.Id} not found.");
+            }
+
+            return order;
+        }
+    }
+}
