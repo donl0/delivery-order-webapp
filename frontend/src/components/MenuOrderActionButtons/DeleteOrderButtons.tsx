71e677491349315b663e5e87143f0684a4b98a2a
donl0 <donl_117n@mail.ru>
2024-09-22 17:00:20 +0300

    Set to delete button actin that affect beckend by deleting order

--- frontend/src/components/MenuOrderActionButtons/DeleteOrderButtons.tsx
+++ frontend/src/components/MenuOrderActionButtons/DeleteOrderButtons.tsx
@@ -1,17 +1,22 @@
+import { deleteOrder } from "../../api/ordersApi";
 import { OrderId } from "../../types/Order";
 import BaseOrderButton from "../UI/orderActoinButton/BaseOrderButton";
 import { FC } from "react";
 
-const DeleteOrderButton : FC<OrderId> = ({id}) => {
-    const handleClick = () => {
-        console.log({id} + "deleted")
-      };
-    
-      return (
-        <BaseOrderButton color="red" onClick={handleClick}>
-          Delete
-        </BaseOrderButton>
-      );
+const DeleteOrderButton: FC<OrderId> = ({ id }) => {
+  const handleClick = () => {
+    const update = async (orderId: number) => {
+      await deleteOrder(orderId);
+    }
+
+    update(id);
+  };
+
+  return (
+    <BaseOrderButton color="red" onClick={handleClick}>
+      Delete
+    </BaseOrderButton>
+  );
 }
 
 export default DeleteOrderButton;
