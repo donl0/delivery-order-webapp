390b7c2d755ce3bf579941dcb58c799dc3f1bacb
donl0 <donl_117n@mail.ru>
2024-09-22 20:05:06 +0300

    Add action to return to main page on form action sucessed

--- frontend/src/utils/orderUtils.ts
+++ frontend/src/utils/orderUtils.ts
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Order } from "../types/Order";
+import { Urls } from "../types/Urls";
 
 export const useNavigateToUrl = () => {
   const navigate = useNavigate();
@@ -10,16 +11,32 @@ export const useNavigateToUrl = () => {
 
 export const mapOrderToDto = (order: Order) => {
   const cargoPickupDate = typeof order.cargoPickupDate === 'string'
-      ? new Date(order.cargoPickupDate)
-      : order.cargoPickupDate;
+    ? new Date(order.cargoPickupDate)
+    : order.cargoPickupDate;
 
   return {
-      id: order.id,
-      senderCity: order.sender.city,
-      senderAddress: order.sender.address,
-      recipientCity: order.recipient.city,
-      recipientAddress: order.recipient.address,
-      cargoWeight: order.cargoWeight,
-      cargoPickupDate: cargoPickupDate.toISOString(),
+    id: order.id,
+    senderCity: order.sender.city,
+    senderAddress: order.sender.address,
+    recipientCity: order.recipient.city,
+    recipientAddress: order.recipient.address,
+    cargoWeight: order.cargoWeight,
+    cargoPickupDate: cargoPickupDate.toISOString(),
   };
+};
+
+export const useTryNavigateIfSuccess = () => {
+  const navigate = useNavigate();
+
+  const tryNavigateIfSuccess = async (action: Function, url: string) => {
+    try {
+      await action();
+      
+      navigate(url);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return tryNavigateIfSuccess;
 };
