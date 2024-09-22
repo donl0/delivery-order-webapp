51195bcc697bd02df833529f62d218fb85fb0ebe
donl0 <donl_117n@mail.ru>
2024-09-22 16:07:39 +0300

    Take order list from beckend

--- frontend/src/api/ordersApi.ts
+++ frontend/src/api/ordersApi.ts
@@ -1,73 +1,38 @@
 import { Order } from '../types/Order';
 
-export const getOrder = async (id: number) : Promise<Order> => {
+export const getOrder = async (id: number): Promise<Order> => {
     let order: Order = {
         id: 1,
-            sender: {
-                city: "Saint-Petersburg",
-                address: "Nevsky",
-            },
-            recipient: {
-                city: "Saint-Petersburg",
-                address: "Nevsky",
-            },
-            cargoWeight: 10,
-            orderNumber: "asdasd32-23233",
-            cargoPickupDate: new Date()
-        };
-    
+        sender: {
+            city: "Saint-Petersburg",
+            address: "Nevsky",
+        },
+        recipient: {
+            city: "Saint-Petersburg",
+            address: "Nevsky",
+        },
+        cargoWeight: 10,
+        orderNumber: "asdasd32-23233",
+        cargoPickupDate: new Date()
+    };
+
     return order;
 }
 
 export const getOrders = async (): Promise<Order[]> => {
-    let data: Order[] = [
-        {
-            id: 1,
-            sender: {
-                city: "Saint-Petersburg",
-                address: "Nevsky",
-            },
-            recipient: {
-                city: "Saint-Petersburg",
-                address: "Nevsky",
-            },
-            cargoWeight: 10,
-            orderNumber: "asdasd32-23233",
-            cargoPickupDate: new Date()
-        },
-        {
-            id: 2,
-            sender: {
-                city: "Saint-Petersburg",
-                address: "Nevsky",
-            },
-            recipient: {
-                city: "Saint-Petersburg",
-                address: "Nevsky",
-            },
-            cargoWeight: 10,
-            orderNumber: "asdasd32-23234",
-            cargoPickupDate: new Date()
-        },
-        {
-            id: 3,
-            sender: {
-                city: "Saint-Petersburg",
-                address: "Nevsky",
-            },
-            recipient: {
-                city: "Saint-Petersburg",
-                address: "Nevsky",
-            },
-            cargoWeight: 10,
-            orderNumber: "asdasd32-2323qweqwe5",
-            cargoPickupDate: new Date()
-        }]
+    const response = await fetch('https://localhost:7292/api/Orders');
 
-    //   const response = await fetch('/api/orders');
-    //   if (!response.ok) {
-    //     throw new Error('Ошибка загрузки заказов');
-    //   }
-    //   const data: Order[] = await response.json();
-    return data;
+    if (!response.ok) {
+        throw new Error('Loading error');
+    }
+
+    const data: Order[] = await response.json();
+
+    const parsedData = data.map(order => ({
+        ...order,
+        cargoPickupDate: new Date(order.cargoPickupDate)
+    }));
+
+    return parsedData;
 };
+
