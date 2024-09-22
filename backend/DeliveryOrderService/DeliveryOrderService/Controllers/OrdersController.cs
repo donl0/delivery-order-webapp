17115ace49d02a54d9cacb247d4a7774d53b65f7
donl0 <donl_117n@mail.ru>
2024-09-22 15:33:24 +0300

    Add mediator to Order Controller Add Update, Delete actions

--- backend/DeliveryOrderService/DeliveryOrderService/Controllers/OrdersController.cs
+++ backend/DeliveryOrderService/DeliveryOrderService/Controllers/OrdersController.cs
@@ -1,6 +1,11 @@
-﻿using Application.DTOs;
-using Application.Interfaces;
+﻿using Application.CQRS.Orders.Commands.CreateOrder;
+using Application.CQRS.Orders.Commands.DeleteOrder;
+using Application.CQRS.Orders.Commands.UpdateOrder;
+using Application.CQRS.Orders.Queries.GetAllOrders;
+using Application.CQRS.Orders.Queries.GetOrder;
+using Application.DTOs;
 using Domain.Models;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DeliveryOrderService.Controllers
@@ -9,37 +14,74 @@ namespace DeliveryOrderService.Controllers
     [ApiController]
     public class OrdersController : ControllerBase
     {
-        private readonly IOrderService _orderService;
+        private readonly IMediator _mediator;
 
-        public OrdersController(IOrderService orderService)
+        public OrdersController(IMediator mediator)
         {
-            _orderService = orderService;
+            _mediator = mediator;
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Order>> GetOrderAsync(long id) {
-            try {
-                Order order = await _orderService.TakeOrderAsync(id);
 
-                return Ok(order);
-            }
-            catch (NotFoundException ex) { 
-                return NotFound();
-            }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrderAsync(long id)
+        {
+            GetOrderQuery query = new GetOrderQuery
+            {
+                Id = id
+            };
+
+            Order order = await _mediator.Send(query);
+
+            return Ok(order);
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Order>>> GetAllOrders() { 
-            IEnumerable<Order> orders = await _orderService.TakeAllOrdersAsync();
+        public async Task<ActionResult<IEnumerable<Order>>> GetAllOrders()
+        {
+            GetAllOrdersQuery query = new GetAllOrdersQuery();
+
+            IEnumerable<Order> orders = await _mediator.Send(query);
 
             return Ok(orders);
         }
 
-        [HttpPost()]
-        public async Task<ActionResult<long>> CreateOrder([FromBody] OrderRequestDTO createTemplate, CancellationToken token) { 
-            long id = await _orderService.CreateOrderAsync(createTemplate, token);
+        [HttpPut()]
+        public async Task<ActionResult<long>> CreateOrder([FromBody] OrderCreateRequestDTO createTemplate, CancellationToken token)
+        {
+            CreateOrderCommand command = new CreateOrderCommand
+            {
+                Order = createTemplate
+            };
+
+            long id = await _mediator.Send(command);
 
             return Ok(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<long>> UpdateOrder([FromBody] OrderUpdateDto order)
+        {
+            UpdateOrderCommand command = new UpdateOrderCommand
+            {
+                Order = order
+            };
+
+            long id = await _mediator.Send(command);
+
+            return Ok(id);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<long>> DeleteOrder(long id)
+        {
+            DeleteOrderCommand command = new DeleteOrderCommand
+            {
+                Id = id
+            };
+
+            long resultId = await _mediator.Send(command);
+
+            return Ok(resultId);
         }
     }
 }
