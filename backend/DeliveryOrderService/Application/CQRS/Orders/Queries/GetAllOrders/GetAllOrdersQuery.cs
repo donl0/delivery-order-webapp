3f200b17ba55b63e3fd55b4568ffbc351a498fd8
donl0 <donl_117n@mail.ru>
2024-09-22 15:30:13 +0300

    Add get all orders via mediator

---
+++ backend/DeliveryOrderService/Application/CQRS/Orders/Queries/GetAllOrders/GetAllOrdersQuery.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models;
+using MediatR;
+
+namespace Application.CQRS.Orders.Queries.GetAllOrders
+{
+    public class GetAllOrdersQuery : IRequest<IEnumerable<Order>>
+    {
+    }
+}
---
+++ backend/DeliveryOrderService/Application/CQRS/Orders/Queries/GetAllOrders/GetAllOrdersQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Domain.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.CQRS.Orders.Queries.GetAllOrders
+{
+    public class GetAllOrdersQueryHandler : IRequestHandler<GetAllOrdersQuery, IEnumerable<Order>>
+    {
+        private readonly IDbContext _context;
+
+        public GetAllOrdersQueryHandler(IDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Order>> Handle(GetAllOrdersQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Order> orders = await _context.Orders.ToListAsync();
+
+            return orders;
+        }
+    }
+}
