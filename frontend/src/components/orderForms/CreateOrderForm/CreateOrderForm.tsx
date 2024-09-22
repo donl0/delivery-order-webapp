26ff7d6336213e04d634de2641867fcb523c9036
donl0 <donl_117n@mail.ru>
2024-09-22 16:54:03 +0300

    Connect create form to beckend via connecting create function

--- frontend/src/components/orderForms/CreateOrderForm/CreateOrderForm.tsx
+++ frontend/src/components/orderForms/CreateOrderForm/CreateOrderForm.tsx
@@ -1,11 +1,17 @@
+import { createOrder } from "../../../api/ordersApi";
 import { Order } from "../../../types/Order";
 import BaseOrderForm from "../abstracts/BaseOrderForm/BaseOrderForm";
 import withFormValidation from "../abstracts/withFormValidation";
 
 const CreateOrderForm = () => {
   const onSubmit = (data: Order) => {
-    console.log("Order created", data);
+    const create = async () => {
+      await createOrder(data);
+    }
+
+    create();
   };
+
 
   const OrderFormWithValidation = withFormValidation(BaseOrderForm);
 
