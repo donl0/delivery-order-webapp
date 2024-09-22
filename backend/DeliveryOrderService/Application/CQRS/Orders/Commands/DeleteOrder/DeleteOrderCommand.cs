e28ebc24196bca350054bce36dcbb1db02e15222
donl0 <donl_117n@mail.ru>
2024-09-22 15:29:38 +0300

    Add delete order via mediator

---
+++ backend/DeliveryOrderService/Application/CQRS/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.CQRS.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommand : IRequest<long>
+    {
+        public long Id { get; set; }
+    }
+}
---
+++ backend/DeliveryOrderService/Application/CQRS/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using Domain.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.CQRS.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand, long>
+    {
+        private readonly IDbContext _context;
+
+        public DeleteOrderCommandHandler(IDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<long> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            Order order = await _context.Orders.FirstOrDefaultAsync(o => o.Id == request.Id);
+
+            if (order == null)
+            {
+                throw new NotFoundException($"Order with id {request.Id}");
+            }
+
+            _context.Orders.Remove(order);
+
+            _context.SaveChangesAsync(cancellationToken);
+
+            return order.Id;
+        }
+    }
+}
